17e99feb954f4ea6eefb5c9b83ee7d7482ab2b2f
Đào Đức Tùng <36580788+daoductung@users.noreply.github.com>
2018-11-25 06:57:20 +0700

    Update wsgi.py

--- pagado/wsgi.py
+++ pagado/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for pagado project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -15,5 +6,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pagado.settings')
 
 application = get_wsgi_application()
-
-
