6e9d61d1ce7f21b2493c991f83c02f53bc187c7a
Phuc Le Cong <lecongphuc92@gmail.com>
2018-11-25 04:07:24 +0700

    Update setting

--- pagado/settings.py
+++ pagado/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
